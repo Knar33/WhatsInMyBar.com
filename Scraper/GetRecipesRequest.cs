f03108ac1faf3e0ed809ee497a102e23a2102d62
Knar33 <adbyerly@comcast.net>
2018-09-16 21:11:55 -0500

    Added Page to request

--- Scraper/GetRecipesRequest.cs
+++ Scraper/GetRecipesRequest.cs
@@ -11,23 +11,24 @@ namespace Scraper
     class GetRecipesRequest
     {
         public string Ingredient { get; set; }
+        public int Page { get; set; }
 
         public GetRecipesResponse Send()
         {
             RestClient client = new RestClient(ConfigurationManager.AppSettings["APIURL"]);
             RestRequest request = new RestRequest(Method.POST);
             request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
-            request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"action\"\r\n\r\nload_search_results\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"ingredients[0]\n\"\r\n\r\n" + Ingredient + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
+            request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"action\"\r\n\r\nload_search_results\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"ingredients[0]\n\"\r\n\r\n" + Ingredient + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"paged\"\r\n\r\n" + Page + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
 
             var res = client.Execute<GetRecipesResponse>(request);
 
             return new GetRecipesResponse(res);
         }
 
-        public GetRecipesRequest(string ingredient)
+        public GetRecipesRequest(string ingredient, int page)
         {
             Ingredient = ingredient;
+            Page = page;
         }
     }
 }
--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -10,13 +10,14 @@ namespace Scraper
     {
         static void Main(string[] args)
         {
-            GetRecipesRequest request = new GetRecipesRequest("Vodka");
+            GetRecipesRequest request = new GetRecipesRequest("Vodka", 1);
             var response = request.Send();
 
             //Load existing ingredient and recipe list from the Database. If no ingredients are found in the database (first scrape), use hard coded list. 
 
             //Iterate over list of ingredients and do a GetRecipeRequest.
-                //Check if the the recipe is already in the Recipes collection. If not, add it.
+                //Calculate how many pages are in the response
+                //Check if the each recipe is already in the Recipes collection. If not, add it.
                     //Check every ingredient in the added recipe to see if it's in the Ingredients collection. If not add it.
                 //Set the Scraped boolean on the ingredient to True
             //Check to see if there any ingrednents that haven't been Scraped. If there are, repeat until there aren't. Otherwise terminate the program.
