fc306a1a5e9ab315d47597f8fd2c3d898374b42d
Knar <knarxr@protonmail.com>
2018-09-16 21:02:03 -0500

    Ingredient constructor for GetRecipesRequest

--- Scraper/GetRecipesRequest.cs
+++ Scraper/GetRecipesRequest.cs
@@ -10,18 +10,24 @@ namespace Scraper
 {
     class GetRecipesRequest
     {
+        public string Ingredient { get; set; }
 
         public GetRecipesResponse Send()
         {
             RestClient client = new RestClient(ConfigurationManager.AppSettings["APIURL"]);
             RestRequest request = new RestRequest(Method.POST);
             request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
-            request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"action\"\r\n\r\nload_search_results\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"ingredients[0]\n\"\r\n\r\nRedbreast\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
+            request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"action\"\r\n\r\nload_search_results\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"ingredients[0]\n\"\r\n\r\n" + Ingredient + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
 
             var res = client.Execute<GetRecipesResponse>(request);
 
             return new GetRecipesResponse(res);
+        }
+
+        public GetRecipesRequest(string ingredient)
+        {
+            Ingredient = ingredient;
         }
     }
 }
