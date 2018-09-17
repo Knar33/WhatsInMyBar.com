9bbe6bdf72371fa24d16765d8fd1369285cb50ff
Knar <knarxr@protonmail.com>
2018-09-16 20:39:10 -0500

    Configure Response object

--- Scraper/GetRecipesRequest.cs
+++ Scraper/GetRecipesRequest.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using System.Configuration;
 
 namespace Scraper
 {
     class GetRecipesRequest
     {
+
+        public GetRecipesResponse Send()
+        {
+            RestClient client = new RestClient(ConfigurationManager.AppSettings["APIURL"]);
+            RestRequest request = new RestRequest(Method.POST);
+
+            var res = client.Execute<GetRecipesResponse>(request);
+
+            return new GetRecipesResponse(res);
+        }
     }
 }
--- Scraper/GetRecipesResponse.cs
+++ Scraper/GetRecipesResponse.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace Scraper
 {
@@ -11,6 +13,28 @@ namespace Scraper
         public int count { get; set; }
         public bool success { get; set; }
         public Recipe[] recipes { get; set; }
+        public string Content { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+        public GetRecipesResponse(IRestResponse<GetRecipesResponse> res)
+        {
+            StatusCode = res.StatusCode;
+            if (res.StatusCode == HttpStatusCode.OK)
+            {
+                count = res.Data.count;
+                success = res.Data.success;
+                recipes = res.Data.recipes;
+            }
+            else
+            {
+                Content = res.Content;
+            }
+        }
+
+        public GetRecipesResponse()
+        {
+
+        }
     }
 
     public class Recipe
