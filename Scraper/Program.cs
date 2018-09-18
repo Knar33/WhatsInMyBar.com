e9028f63b898ec165aaf1c1ad7d03b7c8809c15c
Knar <knarxr@protonmail.com>
2018-09-17 20:29:57 -0500

    Added extra comments for IsNew

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -17,12 +17,13 @@ namespace Scraper
 
             //Iterate over list of ingredients and do a GetRecipeRequest.
                 //Calculate how many pages are in the response
-                //Check if the each recipe is already in the Recipes collection. If not, add it.
+                //Check if the each recipe is already in the Recipes collection. If not, add it and set IsNew = true.
                     //Check every ingredient in the added recipe to see if it's in the Ingredients collection. If not add it.
                 //Set the Scraped boolean on the ingredient to True
             //Check to see if there any ingrednents that haven't been Scraped. If there are, repeat until there aren't. Otherwise terminate the program.
 
             //After all recipes are scraped, do a massive Merge into the database. Merge all Recipes and Ingredients.
+            //for all Recipes with IsNew, download thumbnail image
         }
     }
 }
