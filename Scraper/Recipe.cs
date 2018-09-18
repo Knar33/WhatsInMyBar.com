7a601232c6a8f261bd642ec3f515493f7b26ccbd
Knar <knarxr@protonmail.com>
2018-09-17 20:57:07 -0500

    Broke classes out into their own cs files.

---
+++ Scraper/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scraper
+{
+    public class Recipe
+    {
+        public Recipe()
+        {
+            IsNew = false;
+        }
+
+        public string id { get; set; }
+        public Title title { get; set; }
+        public string link { get; set; }
+        public string thumbnail { get; set; }
+        public List<Ingredient> ingredients { get; set; }
+        public List<Basis> bases { get; set; }
+        public List<Flavor> flavors { get; set; }
+        public string description { get; set; }
+        public bool IsNew { get; set; }
+    }
+}
---
+++ Scraper/Title.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scraper
+{
+    public class Title
+    {
+        public Title()
+        {
+        }
+
+        public string rendered { get; set; }
+    }
+}
