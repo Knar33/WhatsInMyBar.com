2ae536f825c8e085fff22c8b09c8a255baf3edc4
Knar <knarxr@protonmail.com>
2018-09-19 22:11:38 -0500

    Created Database Project

---
+++ WhatsInMyBar.Database/dbo/Tables/Categories.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [CategoryID] INT            NOT NULL,
+    [Name]       VARCHAR (1024) NULL,
+    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([CategoryID] ASC)
+);
+
