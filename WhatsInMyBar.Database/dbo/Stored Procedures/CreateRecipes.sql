a63e4d123cdc2d1522f59fe563c1495d99791432
Knar33 <adbyerly@comcast.net>
2018-09-19 22:33:06 -0500

    Added new stored procedures to sqlproj

---
+++ WhatsInMyBar.Database/dbo/Stored Procedures/CreateRecipes.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[CreateRecipes]
+	@RecipeID INT,
+	@Name Varchar(1024),
+	@Link Varchar(1024),
+	@Thumbnail Varchar(4096),
+	@Description Varchar(4096)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO Recipes (RecipeID, Name, Link, Thumbnail, Description, DateCreated) VALUES (@RecipeID, @Name, @Link, @Thumbnail, @Description, SYSDATETIMEOFFSET())
+	
+END
---
+++ WhatsInMyBar.Database/dbo/Stored Procedures/GetAllRecipes.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetAllRecipes]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Select RecipeID, Name, Link, Thumbnail, Description, DateCreated from Recipes
+	
+END
