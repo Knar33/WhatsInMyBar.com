823615784976db5154fcfe9353cc469527320f0a
Knar33 <adbyerly@comcast.net>
2018-09-19 22:11:38 -0500

    Created Database Project

---
+++ WhatsInMyBar.Database/dbo/Tables/Recipes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Recipes] (
+    [RecipeID]    INT                NOT NULL,
+    [Name]        VARCHAR (1024)     NULL,
+    [Link]        VARCHAR (1024)     NULL,
+    [Thumbnail]   VARCHAR (4096)     NULL,
+    [Description] VARCHAR (4096)     NULL,
+    [DateCreated] DATETIMEOFFSET (7) NULL,
+    CONSTRAINT [PK_Recipes] PRIMARY KEY CLUSTERED ([RecipeID] ASC)
+);
+
