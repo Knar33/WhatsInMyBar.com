6fdf312f2b54338b70c93571b8f648889c6633da
Knar <knarxr@protonmail.com>
2018-09-22 18:50:42 -0500

    Added IngredientCategories table, to assign categories to ingredients

---
+++ WhatsInMyBar.Database/dbo/Tables/IngredientCategories.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[IngredientCategories] (
+    [IngredientID] INT                NOT NULL,
+    [CategoryID]   INT                NULL,
+    [DateCreated]  DATETIMEOFFSET (7) NULL
+);
+
