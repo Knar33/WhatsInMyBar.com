aca4cd633f184573d652b33b57aa63106537ba0b
Knar <knarxr@protonmail.com>
2018-09-23 13:54:56 -0500

    Added GetAllRecipeIngredients stored procedure

---
+++ WhatsInMyBar.Database/dbo/Stored Procedures/GetAllRecipeIngredients.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetAllRecipeIngredients]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Select RecipeID, IngredientID, datecreated from RecipeIngredients
+	
+END
