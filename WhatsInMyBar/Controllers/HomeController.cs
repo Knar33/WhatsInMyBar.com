521b2ee0c9aa24c77b02797b56a402b6dfa9f0e0
Knar33 <adbyerly@comcast.net>
2018-09-23 07:42:53 -0500

    Added thumbnails for drinks

--- WhatsInMyBar/Controllers/HomeController.cs
+++ WhatsInMyBar/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WhatsInMyBar.Models;
 
 namespace WhatsInMyBar.Controllers
 {
     public class HomeController : Controller
     {
+        public List<Recipe> Recipes { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
         public HomeController()
         {
 
