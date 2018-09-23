fb737f21a5e2051c77156fc999f0e1ae50d3fe4b
Knar <knarxr@protonmail.com>
2018-09-23 13:30:19 -0500

    Get objects out of the databse

--- WhatsInMyBar/Controllers/HomeController.cs
+++ WhatsInMyBar/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WhatsInMyBar.Extensions;
 using WhatsInMyBar.Models;
 
 namespace WhatsInMyBar.Controllers
@@ -11,10 +16,119 @@ namespace WhatsInMyBar.Controllers
     {
         public List<Recipe> Recipes { get; set; }
         public List<Ingredient> Ingredients { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<RecipeIngredient> RecipeIngredients { get; set; }
+        public List<IngredientCategory> IngredientCategories { get; set; }
 
         public HomeController()
         {
+            Recipes = new List<Recipe>();
+            Ingredients = new List<Ingredient>();
+            Categories = new List<Category>();
+            RecipeIngredients = new List<RecipeIngredient>();
+            IngredientCategories = new List<IngredientCategory>();
 
+            using (DbConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString))
+            {
+                dbConnection.Open();
+                using (var cmd = dbConnection.CreateCommand())
+                {
+                    cmd.CommandText = "GetAllIngredients";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Ingredients.Add(new Ingredient
+                            {
+                                IngredientID = reader.GetValueOrDefault<int>("IngredientID"),
+                                Name = reader.GetValueOrDefault<string>("Name")
+                            });
+                        }
+                    }
+                }
+
+                using (var cmd = dbConnection.CreateCommand())
+                {
+                    cmd.CommandText = "GetAllRecipes";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Recipes.Add(new Recipe
+                            {
+                                RecipeID = reader.GetValueOrDefault<int>("RecipeID"),
+                                Name = reader.GetValueOrDefault<string>("Name"),
+                                Link = reader.GetValueOrDefault<string>("Link"),
+                                Thumbnail = reader.GetValueOrDefault<string>("Thumbnail"),
+                                Description = reader.GetValueOrDefault<string>("Description")
+                            });
+                        }
+                    }
+                }
+
+                using (var cmd = dbConnection.CreateCommand())
+                {
+                    cmd.CommandText = "GetAllCategories";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Categories.Add(new Category
+                            {
+                                CategoryID = reader.GetValueOrDefault<int>("CategoryID"),
+                                Name = reader.GetValueOrDefault<string>("Name")
+                            });
+                        }
+                    }
+                }
+
+                using (var cmd = dbConnection.CreateCommand())
+                {
+                    cmd.CommandText = "GetAllRecipeIngredients";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            RecipeIngredients.Add(new RecipeIngredient
+                            {
+                                IngredientID = reader.GetValueOrDefault<int>("IngredientID"),
+                                RecipeID = reader.GetValueOrDefault<int>("RecipeID")
+                            });
+                        }
+                    }
+                }
+
+                using (var cmd = dbConnection.CreateCommand())
+                {
+                    cmd.CommandText = "GetAllIngredientCategories";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            IngredientCategories.Add(new IngredientCategory
+                            {
+                                IngredientID = reader.GetValueOrDefault<int>("IngredientID"),
+                                CategoryID = reader.GetValueOrDefault<int>("CategoryID")
+                            });
+                        }
+                    }
+                }
+            }
         }
 
         public ActionResult Index()
