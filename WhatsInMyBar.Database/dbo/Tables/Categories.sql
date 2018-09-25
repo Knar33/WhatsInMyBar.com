6f04cfaee05c0ebe759762bb4d8eeba928aaacd5
Knar <knarxr@protonmail.com>
2018-09-24 20:47:42 -0500

    Added Identity to CategoryID

--- WhatsInMyBar.Database/dbo/Tables/Categories.sql
+++ WhatsInMyBar.Database/dbo/Tables/Categories.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Categories] (
-    [CategoryID] INT            NOT NULL,
+    [CategoryID] INT            IDENTITY (1, 1) NOT NULL,
     [Name]       VARCHAR (1024) NULL,
     CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([CategoryID] ASC)
 );
+
+
 
