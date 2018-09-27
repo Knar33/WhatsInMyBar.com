4b6b2ce9cc2589501edc76dc3c7e0cc88c55e3ba
Knar <knarxr@protonmail.com>
2018-09-27 18:39:07 -0500

    Changed InsertIngredient to be a method in the Ingredient class

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -111,7 +111,7 @@ namespace Scraper
                                             if (!ingredients.Any(x => x.ingredient_id == newIngredient.ingredient_id) && !newIngredients.Any(x => x.ingredient_id == newIngredient.ingredient_id))
                                             {
                                                 newIngredients.Add(newIngredient);
-                                                InsertIngredient(newIngredient);
+                                                newIngredient.Insert();
                                             }
                                             InsertRecipeIngredient(newIngredient, recipe.id);
                                         }
@@ -173,31 +173,7 @@ namespace Scraper
 
 
         
-        public static void InsertIngredient(Ingredient ingredient)
-        {
-            try
-            {
-                using (DbConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString))
-                {
-                    dbConnection.Open();
-                    using (var cmd = dbConnection.CreateCommand())
-                    {
-                        cmd.CommandText = "CreateIngredients";
-                        cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
 
-                        cmd.AddParameter("@IngredientID", ingredient.ingredient_id);
-                        cmd.AddParameter("@Name", ingredient.name);
-
-                        cmd.ExecuteNonQuery();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
 
         public static void InsertRecipeIngredient(Ingredient ingredient, int RecipeID)
         {
@@ -323,76 +299,48 @@ namespace Scraper
         public static List<Ingredient> GetHardCodedIngredients()
         {
             List<Ingredient> ingredients = new List<Ingredient>();
+
             ingredients.Add(new Ingredient(10772, "Absinthe"));
-            InsertIngredient(new Ingredient(10772, "Absinthe"));
             ingredients.Add(new Ingredient(1860, "Brandy"));
-            InsertIngredient(new Ingredient(1860, "Brandy"));
             ingredients.Add(new Ingredient(6630, "Cointreau"));
-            InsertIngredient(new Ingredient(6630, "Cointreau"));
             ingredients.Add(new Ingredient(59, "Grand Marnier"));
-            InsertIngredient(new Ingredient(59, "Grand Marnier"));
             ingredients.Add(new Ingredient(10060, "Orange juice"));
-            InsertIngredient(new Ingredient(10060, "Orange juice"));
             ingredients.Add(new Ingredient(227, "St-Germain "));
-            InsertIngredient(new Ingredient(227, "St-Germain "));
             ingredients.Add(new Ingredient(2043, "Agave nectar"));
-            InsertIngredient(new Ingredient(2043, "Agave nectar"));
             ingredients.Add(new Ingredient(463, "Campari"));
-            InsertIngredient(new Ingredient(463, "Campari"));
             ingredients.Add(new Ingredient(1888, "Cranberry juice"));
-            InsertIngredient(new Ingredient(1888, "Cranberry juice"));
             ingredients.Add(new Ingredient(1922, "Grapefruit juice"));
-            InsertIngredient(new Ingredient(1922, "Grapefruit juice"));
             ingredients.Add(new Ingredient(2010, "Pineapple juice"));
-            InsertIngredient(new Ingredient(2010, "Pineapple juice"));
             ingredients.Add(new Ingredient(1872, "Sweet vermouth"));
-            InsertIngredient(new Ingredient(1872, "Sweet vermouth"));
             ingredients.Add(new Ingredient(2198, "Aperol"));
-            InsertIngredient(new Ingredient(2198, "Aperol"));
             ingredients.Add(new Ingredient(1910, "Champagne"));
-            InsertIngredient(new Ingredient(1910, "Champagne"));
             ingredients.Add(new Ingredient(10307, "Dry vermouth"));
-            InsertIngredient(new Ingredient(10307, "Dry vermouth"));
             ingredients.Add(new Ingredient(2049, "Green Chartreuse"));
-            InsertIngredient(new Ingredient(2049, "Green Chartreuse"));
             ingredients.Add(new Ingredient(7433, "Pisco"));
-            InsertIngredient(new Ingredient(7433, "Pisco"));
             ingredients.Add(new Ingredient(10276, "Tequila"));
-            InsertIngredient(new Ingredient(10276, "Tequila"));
             ingredients.Add(new Ingredient(268, "Bénédictine"));
-            InsertIngredient(new Ingredient(268, "Bénédictine"));
             ingredients.Add(new Ingredient(1947, "Club soda"));
-            InsertIngredient(new Ingredient(1947, "Club soda"));
             ingredients.Add(new Ingredient(1867, "Egg white"));
-            InsertIngredient(new Ingredient(1867, "Egg white"));
             ingredients.Add(new Ingredient(468, "Grenadine"));
-            InsertIngredient(new Ingredient(468, "Grenadine"));
             ingredients.Add(new Ingredient(10516, "Rum"));
-            InsertIngredient(new Ingredient(10516, "Rum"));
             ingredients.Add(new Ingredient(1892, "Triple sec"));
-            InsertIngredient(new Ingredient(1892, "Triple sec"));
             ingredients.Add(new Ingredient(1937, "Coffee liqueur "));
-            InsertIngredient(new Ingredient(1937, "Coffee liqueur "));
             ingredients.Add(new Ingredient(10096, "Gin"));
-            InsertIngredient(new Ingredient(10096, "Gin"));
             ingredients.Add(new Ingredient(1865, "Lemon juice"));
-            InsertIngredient(new Ingredient(1865, "Lemon juice"));
             ingredients.Add(new Ingredient(10334, "Scotch"));
-            InsertIngredient(new Ingredient(10334, "Scotch"));
             ingredients.Add(new Ingredient(1966, "Vodka"));
-            InsertIngredient(new Ingredient(1966, "Vodka"));
             ingredients.Add(new Ingredient(6568, "Bourbon"));
-            InsertIngredient(new Ingredient(6568, "Bourbon"));
             ingredients.Add(new Ingredient(696, "Cognac"));
-            InsertIngredient(new Ingredient(696, "Cognac"));
             ingredients.Add(new Ingredient(1915, "Ginger beer"));
-            InsertIngredient(new Ingredient(1915, "Ginger beer"));
             ingredients.Add(new Ingredient(1893, "Lime juice"));
-            InsertIngredient(new Ingredient(1893, "Lime juice"));
             ingredients.Add(new Ingredient(19, "Simple syrup"));
-            InsertIngredient(new Ingredient(19, "Simple syrup"));
             ingredients.Add(new Ingredient(1935, "Rye whiskey"));
-            InsertIngredient(new Ingredient(1935, "Rye whiskey"));
+
+            foreach (Ingredient ingredient in ingredients)
+            {
+                ingredient.Insert();
+            }
+
             return ingredients;
         }
     }
