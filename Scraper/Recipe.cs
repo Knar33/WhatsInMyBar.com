63ba78b388ec9532f258249f8509435431a74fd7
Knar <knarxr@protonmail.com>
2018-09-27 18:29:56 -0500

    Changed InsertRecipe to be a method in the Recipe class

--- Scraper/Recipe.cs
+++ Scraper/Recipe.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+using WhatsInMyBar.Extensions;
 
 namespace Scraper
 {
@@ -16,5 +18,34 @@ namespace Scraper
         public List<Basis> bases { get; set; }
         public List<Flavor> flavors { get; set; }
         public string description { get; set; }
+
+        public void Insert()
+        {
+            try
+            {
+                using (DbConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString))
+                {
+                    dbConnection.Open();
+                    using (var cmd = dbConnection.CreateCommand())
+                    {
+                        cmd.CommandText = "CreateRecipes";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
+
+                        cmd.AddParameter("@RecipeID", id);
+                        cmd.AddParameter("@Name", title.rendered);
+                        cmd.AddParameter("@Link", link);
+                        cmd.AddParameter("@Thumbnail", thumbnail);
+                        cmd.AddParameter("@Description", description);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
