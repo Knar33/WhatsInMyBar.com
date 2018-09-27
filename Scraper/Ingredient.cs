5538eb81aee1a4c3238be5ced05c0767d6ef2e01
Knar33 <adbyerly@comcast.net>
2018-09-27 18:39:07 -0500

    Changed InsertIngredient to be a method in the Ingredient class

--- Scraper/Ingredient.cs
+++ Scraper/Ingredient.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhatsInMyBar.Extensions;
 
 namespace Scraper
 {
@@ -24,5 +29,31 @@ namespace Scraper
         public int? Category { get; set; }
         public string name { get; set; }
         public bool Scraped { get; set; }
+
+        public void Insert()
+        {
+            try
+            {
+                using (DbConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString))
+                {
+                    dbConnection.Open();
+                    using (var cmd = dbConnection.CreateCommand())
+                    {
+                        cmd.CommandText = "CreateIngredients";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
+
+                        cmd.AddParameter("@IngredientID", ingredient_id);
+                        cmd.AddParameter("@Name", name);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
