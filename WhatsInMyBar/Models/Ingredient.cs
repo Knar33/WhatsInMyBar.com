bdc57aa3ca2b0b2ce25e6a7fb12f5001a303ec0f
Knar33 <adbyerly@comcast.net>
2018-10-01 22:16:31 -0500

    Added GetIngredientsFromDatabase method to Ingredient class

--- WhatsInMyBar/Models/Ingredient.cs
+++ WhatsInMyBar/Models/Ingredient.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using WhatsInMyBar.Extensions;
 
 namespace WhatsInMyBar.Models
 {
@@ -11,5 +16,42 @@ namespace WhatsInMyBar.Models
         public string Name { get; set; }
         public DateTimeOffset DateCreated { get; set; }
         public List<Category> Categories { get; set; }
+
+        public static List<Ingredient> GetIngredientsFromDatabase()
+        {
+            List<Ingredient> ingredients = new List<Ingredient>();
+
+            try
+            {
+                using (DbConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString))
+                {
+                    dbConnection.Open();
+                    using (var cmd = dbConnection.CreateCommand())
+                    {
+                        cmd.CommandText = "GetAllIngredients";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandTimeout = Int32.Parse(ConfigurationManager.AppSettings["SQLTimeout"]);
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ingredients.Add(new Ingredient
+                                {
+                                    IngredientID = reader.GetValueOrDefault<int>("IngredientID"),
+                                    Name = reader.GetValueOrDefault<string>("Name")
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return ingredients;
+        }
     }
 }
