cc63931517266b918392ec4b513e4e4d5b95febc
Knar33 <adbyerly@comcast.net>
2018-10-21 17:52:46 -0500

    Added DateUpdated column to Ingredients table - This will be used when classifying ingredients

--- WhatsInMyBar.Database/dbo/Tables/Ingredients.sql
+++ WhatsInMyBar.Database/dbo/Tables/Ingredients.sql
@@ -2,8 +2,11 @@
     [IngredientID] INT                NOT NULL,
     [Name]         VARCHAR (1024)     NULL,
     [DateCreated]  DATETIMEOFFSET (7) NULL,
+    [DateUpdated]  DATETIMEOFFSET (7) NULL,
     CONSTRAINT [PK_Ingredients] PRIMARY KEY CLUSTERED ([IngredientID] ASC)
 );
+
+
 
 
 
